54599ee15908983c019706cb8abd28e3432863d6
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-08-20 01:09:32 +0500

    Addfavorite and remove favorite completed

--- Chinook/Services/IServices/ITrackService.cs
+++ Chinook/Services/IServices/ITrackService.cs
@@ -6,5 +6,8 @@ namespace Chinook.Services.IServices
     {
         Task<Track?> GetTrackByIdAsync(long trackId);
         Task<IEnumerable<Track>> GetTracksWithAlbumInfoByArtistIdAsync(long artistId);
+        Task<List<Track>> GetFavoriteTracksByUserIdAsync(string userId);
+        Task UpdateTrackAsFavorite(string userId, long trackId);
+        Task UpdateTrackAsUnFavorite(string userId, long trackId);
     }
 }
--- Chinook/Services/TrackService.cs
+++ Chinook/Services/TrackService.cs
@@ -11,6 +11,7 @@ namespace Chinook.Services
         {
             _dbContext = dbContext;
         }
+
         public async Task<Track?> GetTrackByIdAsync(long trackId)
         {
             return await _dbContext.Tracks.FirstOrDefaultAsync(t => t.TrackId == trackId);
@@ -19,7 +20,90 @@ namespace Chinook.Services
         public async Task<IEnumerable<Track>> GetTracksWithAlbumInfoByArtistIdAsync(long artistId)
         {
             return await _dbContext.Tracks.Where(a => a.Album.ArtistId == artistId)
-                .Include(a => a.Album).ToListAsync();
+                .Include(a => a.Album)
+                .ToListAsync();
+        }
+
+        public async Task<List<Track>> GetFavoriteTracksByUserIdAsync(string userId)
+        {
+            var favoritePlayList = await _dbContext.UserPlaylists
+                .Include(r => r.Playlist)
+                .ThenInclude(r => r.Tracks)
+                .FirstOrDefaultAsync(r => r.UserId == userId && r.Playlist.Name == "Favorites");
+
+            return favoritePlayList?.Playlist?.Tracks?.ToList();
+        }
+
+        public async Task UpdateTrackAsUnFavorite(string userId, long trackId)
+        {
+            try
+            {
+                var favoritePlaList = await _dbContext.UserPlaylists
+                   .Include(r => r.Playlist)
+                   .ThenInclude(r => r.Tracks)
+                   .FirstOrDefaultAsync(r => r.UserId == userId && r.Playlist.Name == "Favorites");
+
+                var track = await GetTrackByIdAsync(trackId);
+
+                var trakIndb = favoritePlaList.Playlist.Tracks.SingleOrDefault(r => r.TrackId == trackId);
+                if (trackId != null)
+                {
+                    favoritePlaList.Playlist.Tracks.Remove(track);
+                    await _dbContext.SaveChangesAsync();
+                }
+
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public async Task UpdateTrackAsFavorite(string userId, long trackId)
+        {
+            try
+            {
+                var favoritePlaList = await _dbContext.UserPlaylists
+                .Include(r => r.Playlist)
+                .ThenInclude(r => r.Tracks)
+                .FirstOrDefaultAsync(r => r.UserId == userId && r.Playlist.Name == "Favorites");
+
+                var track = await GetTrackByIdAsync(trackId);
+
+                if (favoritePlaList != null)
+                {
+                    if (favoritePlaList.Playlist.Tracks == null)
+                    {
+                        favoritePlaList.Playlist.Tracks = new List<Track>();
+                        favoritePlaList.Playlist?.Tracks.Add(track);
+                        await _dbContext.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        var trakIndb = favoritePlaList.Playlist.Tracks.SingleOrDefault(r => r.TrackId == trackId);
+                        if (trakIndb == null)
+                        {
+                            favoritePlaList.Playlist.Tracks.Add(track);
+                            await _dbContext.SaveChangesAsync();
+                        }
+                    }
+                }
+                else
+                {
+                    var favPlayList = new Models.Playlist() { Name = "Favorites" };
+                    favPlayList.Tracks.Add(track);
+                    await _dbContext.Playlists.AddAsync(favPlayList);
+                    await _dbContext.SaveChangesAsync();
+
+                    var userPlayList = new UserPlaylist() { PlaylistId = favPlayList.PlaylistId, UserId = userId };
+                    await _dbContext.UserPlaylists.AddAsync(userPlayList);
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
         }
     }
 }
